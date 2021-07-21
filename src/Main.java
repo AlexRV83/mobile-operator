399377a9928049144c7e28063c741d97e633648c
AlexRV83 <85925901+AlexRV83@users.noreply.github.com>
2021-07-21 23:17:22 +0300

    revision 1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main<bonus> {
     public static void main(String[] args) {
-        int account = 156;
-        int refill = 1515;
-        int bonus = refill/100;
+        int account = 325;
+        int refill = 999;
+        int bonus = refill / 100;
         int total;
 
 
         if (refill > 1000) {
             total = account + refill + bonus;
         } else {
-             total = account + refill;
+            total = account + refill;
+            bonus = 0;
         }
         System.out.println(total);
+        System.out.println(bonus);
     }
 }
